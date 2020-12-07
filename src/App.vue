bd821173500b593632506dc29b0fa9df62326b64
Joachim Holwech <joachim.holwech@gmail.com>
2020-12-07 08:21:05 +0100

    Add router injection to App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,41 +3,22 @@
 </template>
 
 <script lang="ts">
-import MainMenu from "@/layouts/MainMenu.vue";
+import MainMenu from '@/layouts/MainMenu.vue';
 import { defineComponent, provide } from '@vue/composition-api';
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import Auth, { IUserFlows } from './utils/Auth';
 import { Configuration } from 'msal';
 
-const config: Configuration = {
-  auth: {
-    clientId: 'f8b8ed45-8ac1-442d-961d-c6efb6f2413b',
-    authority:
-      'https://incrementally.b2clogin.com/incrementally.onmicrosoft.com/B2C_1_signin_or_signup',
-    validateAuthority: false
-  },
-  cache: {
-    cacheLocation: 'localStorage',
-    storeAuthStateInCookie: true
-  }
-};
-const userFlows: IUserFlows = {
-  signin:
-    'https://incrementally.b2clogin.com/incrementally.onmicrosoft.com/B2C_1_signin_or_signup',
-  signup:
-    'https://incrementally.b2clogin.com/incrementally.onmicrosoft.com/B2C_1_signup'
-};
-const auth = new Auth(config, userFlows);
-Vue.prototype.$auth = auth;
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
     MainMenu
   },
   setup() {
-    provide('auth', auth);
+    provide('auth', Vue.prototype.$auth);
+    provide('router', Vue.prototype.$routerHandle);
   }
-})
+});
 </script>
