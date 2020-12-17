5fc533c1d39f43b6104dc029f5610903cfb0adb3
Joachim Holwech <joachim.holwech@gmail.com>
2020-12-17 07:38:36 +0100

    Improve resource view

--- src/layouts/ResourceView.vue
+++ src/layouts/ResourceView.vue
@@ -1,31 +1,56 @@
 <template>
-  <b-spinner
-    v-if="getEntryLoading"
-    label="Loading..."
-    size="lg"
-    style="width: 3rem; height: 3rem;"
-  />
-  <div v-else>
-    <b-card
-      :title="entry.title"
-      class="result-explore"
-    >
-      <b-card-text>
-        {{ entry.description }}
-        {{ entry.givenName }}
-        {{ entry.createdAt }}
-      </b-card-text>
-
+  <div>
+    <Toolbar>
       <b-button
-        v-if="canDelete(entry.createdBy)"
-        v-b-modal.delete-entry
-        variant="outline-dark"
-        style="margin-left: 5px"
-        @click="openDeleteModal(entry.id)"
+        v-b-modal.modal-save-dialog
+        variant="light"
+        class="editor-button"
       >
-        Delete
+        <b-icon
+          icon="plus-circle"
+          variant="dark"
+        />
       </b-button>
-    </b-card>
+      <SaveDialog modal-id="modal-save-dialog" />
+      <LoginButton />
+    </Toolbar>
+    <HelpDialog />
+    <b-container id="main-container">
+      <b-row
+        align-h="center"
+        style="margin-bottom: 50px;"
+      >
+        <b-col cols="8">
+          <b-spinner
+            v-if="getEntryLoading"
+            label="Loading..."
+            size="lg"
+            style="width: 3rem; height: 3rem;"
+          />
+          <b-card
+            :title="entry.title"
+            :sub-title="dateToDateString(entry.createdAt)"
+            class="result-explore"
+            v-else
+          >
+            <b-card-text>
+              {{ entry.description }}
+              {{ entry.givenName }}
+            </b-card-text>
+
+            <b-button
+              v-if="canDelete(entry.createdBy)"
+              v-b-modal.delete-entry
+              variant="outline-dark"
+              style="margin-left: 5px"
+              @click="openDeleteModal(entry.id)"
+            >
+              Delete
+            </b-button>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -35,12 +60,24 @@ import { SaveDialogFormTemplate } from '@/models/SaveDialogForm';
 import Auth from '@/utils/Auth';
 import { useDeleteEntry, useGetEntry } from '@/utils/Api';
 import Router from 'vue-router';
+import LoginButton from '@/components/LoginButton.vue';
+import Toolbar from '@/layouts/Toolbar.vue';
+import SaveDialog from '@/components/SaveDialog.vue';
+import HelpDialog from '@/components/HelpDialog.vue';
+import { dateToDateString } from '@/utils/formatters';
 
 export default defineComponent({
   name: 'ResourceView',
+  components: {
+    LoginButton,
+    Toolbar,
+    SaveDialog,
+    HelpDialog
+  },
   setup() {
     let selectedDelete = ref('');
     let entry = ref({});
+    let date = ref();
     const auth = inject('auth') as Auth;
     const router = inject('router') as Router;
     const { deleteEntry, deleteEntryLoading } = useDeleteEntry(auth, selectedDelete);
@@ -48,8 +85,6 @@ export default defineComponent({
 
     onMounted(async () => {
       entry.value = await getEntry();
-      console.log(entry.value);
-      console.log(getEntryLoading);
     });
 
     const canDelete = (id: string) => {
@@ -64,6 +99,7 @@ export default defineComponent({
       deleteEntry,
       deleteEntryLoading,
       canDelete,
+      dateToDateString
     };
   }
 });
